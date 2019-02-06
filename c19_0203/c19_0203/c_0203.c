5f8ac6fee7c783b4020b362a150a36e2eb3fe56e
ForYan <2390523320@qq.com>
2019-02-06 18:55:26 +0800

    修改

--- c19_0203/c19_0203/c_0203.c
+++ c19_0203/c19_0203/c_0203.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
+int main(){
 	int choice = 0;
-	printf("请输入你的选择：0 会好好学习 1 我还没玩够\n");
+	printf("请输入你的选择:(1 or 0)");
 	scanf("%d", &choice);
-	if (choice == 0){
-		printf("会上一个好高中\n");
-
+	if (choice == 1){
+		printf("好offer在向你招手！\n");
+	}
+	else if (choice == 0){
+		printf("回家卖红薯吧！\n");
 	}
 	else{
-		printf("回家算柿饼吧\n");
+		printf("错误输入\n");
 	}
 	system("pause");
 	return 0;
